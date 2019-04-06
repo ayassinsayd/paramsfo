531715a3bf4fe01e999ea3a538e9ab836b13bf97
a.yassin <a.yassin@msn.com>
2019-04-06 17:46:34 +0200

    Update project files.

--- paramsfo/main.cpp
+++ paramsfo/main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	PARAM("c:\\x.sfo").insert("target_app_ver","00.01",8);
+	PARAM("c:\\x.sfo").remove("target_app_ver");
+	PARAM("c:\\x.sfo").insert("target_app_ver", "00.01", 8);
 	qDebug() << "finish";
 	getchar();
 	//return a.exec();
